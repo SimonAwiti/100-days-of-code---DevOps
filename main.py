4babf6d0016b5aebf5f969dbe5fbed9b87991c63
simonawiti <awitisimon23@gmail.com>
2025-02-12 12:23:50 +0100

    typecasting

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-#variables 
+#vTypecasting: converting to str() float() int() bool()
 
 age = 29
 name = "Simon Awiti"
+is_student = False 
+gpa = 4.5
 
-print (f"my name is {name} and I'm {age} years old")
+if is_student:
+    print (f"my name is {name} and I'm {age} years old and Im a student")
+else:
+    print (f"my name is {name} and I'm {age} years old and Im Not a student")
+    
+    
+print (type(age))
+
+gpa = int(gpa)
+
+print (gpa)
+
+#typecasting a string into a bool 
+
+name = bool(name)
+
+print (name)

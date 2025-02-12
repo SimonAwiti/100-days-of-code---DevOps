8337db836dcbf81a30efaec9f0e68d936a1ef2e5
simonawiti <awitisimon23@gmail.com>
2025-02-12 17:52:54 +0100

    area of a circle

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# A game of madlibs
-# You enter words to make up a story by filling in the blanks
-noun1 = input("Enter a noun: ")
-verb1 = input("Enter a varb in past tense: ")
+# calculating the area of a circle 
 
-print ("below is your story")
-print(f"There was once a {noun1} who was friends with a tortoise. One day, he {verb1} the tortoise to a race. Seeing how slow the tortoise was going, the {noun1} thought hed win this easily. So, he took a nap while the tortoise kept on going. When the {noun1} woke, he saw that the tortoise was already at the finish line. Much to his chagrin, the tortoise won the race while he was busy sleeping.")
+import math
 
+r = 7
+
+area = math.pi * math.pow(r, 2)
+
+print (area)

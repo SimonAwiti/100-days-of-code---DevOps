0d6829b027d6f2bc2c435605dbf99bdd915abfca
simonawiti <awitisimon23@gmail.com>
2025-02-12 17:38:35 +0100

    arithmetics

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 #input 
-name = input("What is your name? : ")
-age = int(input(f"Hi {name}, how old are you? : ")) #typecast the input into an int 
+#name = input("What is your name? : ")
+#age = int(input(f"Hi {name}, how old are you? : ")) #typecast the input into an int 
 
+age = 20
 age = age + 1
 
-print(f"Hello {name}, HAPPY BIRTHDAY! You are {age} years old")
+#print(f"Hello {name}, HAPPY BIRTHDAY! You are {age} years old")
+
+#arithmetics 
+
+#Addition 
+age += 1  
+age -= 1
+age *= 2
+age /= 2
+age %= 2
+
+#print (age)
+
+#builtin functions
+x = 2.2
+y = 3
+z = 5
+
+result = round(x)
+print(result)
+
+result2 = abs(y)
+print(result2)
+
+result3 = pow(y, 2)
+print(result3)
+
+result4 = max(x, y, z)
+print(result4)
+
+result5 = min(x, y, z)
+print(result5)

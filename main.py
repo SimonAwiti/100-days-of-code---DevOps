7ec04d5732b7b509dc8863b3a906920b0d540abd
Simon Awiti <42590964+SimonAwiti@users.noreply.github.com>
2025-02-17 14:42:54 +0300

    conditional expressions

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 # if statements 
 
+#conditional expression
+
 age = int(input("Enter your age: "))
 
-if age >= 50:
-    print("You are too old for this man!")
-elif age == 30:
-    print("Thats the parfect age")
-else:
-    print("You are not 30 yet")
+print("too old" if age > 50 else "still viable")
+
 
 

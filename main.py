ee2484eff6bc197c29447837accece02404b8246
Simon Awiti <42590964+SimonAwiti@users.noreply.github.com>
2025-02-20 23:54:12 +0300

    methods in lists

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 
 fruits = ["banana", "apple", "pineable"]
 fruits.append("avocado") #adds an element in a list
+fruits.remove("banana")
 fruits[2]= "pineapple" #changes an element in a list
+
+fruits.insert(2, "coconout")
+fruits.sort()
+fruits.reverse()
 
 print("banana" in fruits) # returns boolean 
 
 for fruit in fruits:
     
-    print (fruits[:3])
+    print (fruits)
     print(len(fruits)) #print the length of the list
    
 

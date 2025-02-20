4f36075136227e256ea78d3881234e15b287974c
Simon Awiti <42590964+SimonAwiti@users.noreply.github.com>
2025-02-20 23:46:10 +0300

    more about lists

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #collections used to sto several entries into a single variable
 
 fruits = ["banana", "apple", "pineable"]
-fruits.append("avocado")
+fruits.append("avocado") #adds an element in a list
+fruits[2]= "pineapple" #changes an element in a list
+
+print("banana" in fruits) # returns boolean 
 
 for fruit in fruits:
     
-    print (fruits[3])
+    print (fruits[:3])
+    print(len(fruits)) #print the length of the list
    
 

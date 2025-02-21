5dfac4b5b4fe11bf7fd398de37236cb4f7ca87c0
Simon Awiti <42590964+SimonAwiti@users.noreply.github.com>
2025-02-21 19:21:14 +0300

    Sets

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-#collections used to sto several entries into a single variable
+#sets in python 
+fruits = ["coconut", "dates"]
+set = {"apple", "bananas", "avocado"}
+#set2 = set(fruits)
 
-fruits = ["banana", "apple", "pineable"]
-fruits.append("avocado") #adds an element in a list
-fruits.remove("banana")
-fruits[2]= "pineapple" #changes an element in a list
-
-fruits.insert(2, "coconout")
-fruits.sort()
-fruits.reverse()
-
-print("banana" in fruits) # returns boolean 
-
-for fruit in fruits:
-    
-    print (fruits)
-    print(len(fruits)) #print the length of the list
-   
-
+print (set)
